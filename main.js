98181b401bb535237ad92694dc5c9b03f066f814
Ollie Wells <ollie@olliewells.co.uk>
2025-04-11 21:56:50 +0000

    index

---
+++ main.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const volume = document.getElementById("volumeInput").value
+
+    alert(volume)
+
+})
